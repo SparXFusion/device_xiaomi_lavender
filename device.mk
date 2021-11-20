271b7dc02df391c7d51bf2f3276e4093ce215715
faham1997 <nafidfaham08@gmail.com>
2021-11-21 00:47:08 +0600

    lavender: Switch to Audio HAL V6

--- device.mk
+++ device.mk
@@ -75,8 +75,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl\
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl\
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
